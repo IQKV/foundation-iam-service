d1e164d01996bb26c8a32c328c197c0b98086825
Dima Denisenko <1253839+dimdnk@users.noreply.github.com>
2026-05-01 15:46:35 +0300

    style: remove unused imports in tests

--- src/test/java/com/iqkv/foundation/iamservice/authentication/dto/AuthenticationDtosTest.java
+++ src/test/java/com/iqkv/foundation/iamservice/authentication/dto/AuthenticationDtosTest.java
@@ -18,7 +18,6 @@ package com.iqkv.foundation.iamservice.authentication.dto;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 
